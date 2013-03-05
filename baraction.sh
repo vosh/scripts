8206bec7fbbe37b3ef7a3e735762e7d127487537
vosh <bdrolshagen@gmail.com>
2013-03-05 20:34:28 +0100

    Update baraction.sh

--- baraction.sh
+++ baraction.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
-conky | dzen2 -fg "#9999CC" -bg "#252627" -ta left -w 1000 -h 0 -x 220 -y 2 -fn -*-terminus-*-*-*-*-12-*-*-*-*-*-*-* &
+wmname spectrwm
+conky -c ~/.conkyrc2| dzen2 -fg "#9999CC" -bg "#252627" -ta left -w 1366 -h 0 -x 55 -y 2 -fn -*-terminus-*-*-*-*-12-*-*-*-*-*-*-* &
 exit 0
